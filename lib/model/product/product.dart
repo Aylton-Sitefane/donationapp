a6f0e585d52be49a6b07139b5ea24662da1b3833
Aylton-Sitefane <aylton.sitefane1@gmail.com>
2024-09-17 13:18:47 +0200

    criacao da entidade produto

--- lib/model/product/product.dart
+++ lib/model/product/product.dart
@@ -2,17 +2,23 @@ class Product{
   String? name;
   String? description;
   String? sId;
+  String? quantity;
+  bool? deliver;
 
   Product({
     this.sId,
     this.name,
-    this.description
+    this.description,
+    this.quantity,
+    this.deliver,
   });
 
   Product.fromJson(Map<String, dynamic> json) {
     sId = json['_id'];
     name = json['name'];
     description = json['description'];
+    quantity = json['quantity'];
+    deliver = json['deliver'];
 
   }
 
@@ -21,13 +27,15 @@ class Product{
     data['_id'] = sId;
     data['name'] = name;
     data['description'] = description;
+    data['quantity'] = quantity;
+    data['deliver'] = deliver;
     return data;
   }
 
-   List<Product> productList =[
-    Product(name: 'Bananas', description: 'Proveniente de maxixe dispon]ivel patadjksbkqs'),
-    Product(name: 'Bananas', description: 'Proveniente de maxixe dispon]ivel patadjksbkqs'),
-    Product(name: 'Bananas', description: 'Proveniente de maxixe dispon]ivel patadjksbkqs'),
+   static List<Product> productList =[
+    Product(name: 'Bananas', description: 'Proveniente de maxixe dispon]ivel patadjksbkqs', quantity: '2'),
+    Product(name: 'Laranjas', description: 'Proveniente de maxixe dispon]ivel patadjksbkqs',  quantity: '33'),
+    Product(name: 'Cesta basica', description: 'Proveniente de maxixe dispon]ivel patadjksbkqs', quantity: '44'),
   ];
 
 
