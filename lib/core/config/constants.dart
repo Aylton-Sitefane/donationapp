c5e76fdaff51385e3e480654fe02025118e361c4
Aylton-Sitefane <aylton.sitefane1@gmail.com>
2024-09-17 13:17:53 +0200

    adicao da constante da url

--- lib/core/config/constants.dart
+++ lib/core/config/constants.dart
@@ -1,15 +1,1 @@
-class Items {
-  String name;
-  String description;
-
-
-  Items({
-    required this.name,
-    required this.description,
-});
-
- static List<Items> items = [
-   Items(name: "Cesta basica", description: "Da igreja bla bla bla"),
-   Items(name: "Maçã", description: "colhido no jardim bla bla bla"),
- ];
-}
+const MAIN_URL = 'http://localhost:3000';
