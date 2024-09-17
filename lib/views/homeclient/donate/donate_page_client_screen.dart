7e844139ef8eafb260f3cc3869b1cf0d1a8d810c
Aylton-Sitefane <aylton.sitefane1@gmail.com>
2024-09-17 16:47:44 +0200

    Estilizacao da pagina que lista as doacoes feitas e recebidas

--- lib/views/homeclient/donate/donate_page_client_screen.dart
+++ lib/views/homeclient/donate/donate_page_client_screen.dart
@@ -2,6 +2,8 @@ import 'package:donationapp/core/config/theme/app-color.dart';
 import 'package:donationapp/views/homeclient/donate/done/donation_completed_list.dart';
 import 'package:donationapp/views/homeclient/donate/progress/donation_progress_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import '../../../core/config/assets/app_vectors.dart';
 
 class DonatePageClientScreen extends StatefulWidget {
   const DonatePageClientScreen({super.key});
@@ -30,38 +32,83 @@ class _DonatePageClientScreenState extends State<DonatePageClientScreen>
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Donate Page'),
+        automaticallyImplyLeading: false,
+        backgroundColor: AppColor.appbarColor,
+        toolbarHeight: 150,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(20),
+            bottomRight: Radius.circular(20),
+          ),
+        ),
+        title: Column(
+          children: [
+            const SizedBox(height: 70),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SvgPicture.asset(
+                  AppVectors.donateHeart,
+                  height: 40,
+                ),
+                Container(
+                  height: 50,
+                  width: 240,
+                  decoration: BoxDecoration(
+                    color: AppColor.lightBackground,
+                    borderRadius: BorderRadius.circular(22),
+                  ),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Pesquisar...',
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.all(10),
+                    ),
+                  ),
+                ),
+                const Icon(
+                  Icons.logout,
+                  color: AppColor.lightBackground,
+                  size: 30,
+                ),
+              ],
+            ),
+          ],
+        ),
         bottom: TabBar(
           controller: _tabController,
-          labelColor: Colors.white,
-          unselectedLabelColor: Colors.white70,
-          indicatorColor: AppColor.primaryColor,
-          indicatorWeight: 3.0,
           tabs: const [
-            Tab(text: 'Doações feitas'),
-            Tab(text: 'Doações recebidas'),
+            Tab(child: SizedBox(width: 120,child: Center(child: Text('Doacoes feitas', style: TextStyle(fontWeight: FontWeight.bold,),))),),
+            Tab(child: SizedBox(width: 150,child: Center(child: Text('Doacoes recebidas', style: TextStyle(fontWeight: FontWeight.bold),))),),
           ],
+          indicator: BoxDecoration(
+    color:AppColor.lightBackground,
+    borderRadius: const BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)), // Borda arredondada
+    boxShadow: [
+       
+      BoxShadow(
+        
+        color: Colors.black.withOpacity(0.3),
+        spreadRadius: 1,
+        blurRadius: 5,
+        offset: const Offset(0, 2), // Posição da sombra
+      ),
+    ],
+  ),
+  //labelColor: Colors.white,
+  unselectedLabelColor: Colors.grey,
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: TabBarView(
-          controller: _tabController,
-          children: const <Widget>[
-            Card(
-              margin: EdgeInsets.all(16.0),
-              child: Center(
-                child: DonationProgressList(),
-              ),
-            ),
-            Card(
-              margin: EdgeInsets.all(16.0),
-              child: Center(
-                child: DonationCompletedList(),
-              ),
-            ),
-          ],
-        ),
+      body: TabBarView(
+        controller: _tabController,
+        children: const [
+          Card(
+            child: DonationProgressList(),
+          ),
+          Card(
+            child: DonationCompletedList(),
+          ),
+        ],
       ),
     );
   }
